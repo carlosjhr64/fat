d3e62cf4ecba28c3027cfd4ff69facadb16c8731
carlosjhr64 <carlosjhr64@gmail.com>
2016-02-01 12:29:18 -0800

    Agglomerate, versio 0.3.0

--- README.txt
+++ README.txt
@@ -1,8 +1,9 @@
 package fat // import "github.com/carlosjhr64/fat"
 
 Float Array Tricks
-var VERSION string = "0.2.0"
+var VERSION string = "0.3.0"
 func Cluster(scatter []float64, delta float64) []float64
+func Agglomerate(scatter []float64, delta float64) []float64
 func Copy(a []float64) []float64
 func Join(a []float64, format string, sep string) string
 func SortByNear(s []float64, n float64)
--- fat.go
+++ fat.go
@@ -5,7 +5,7 @@ import "math"
 import "fmt"
 import "sort"
 
-var VERSION string = "0.2.0"
+var VERSION string = "0.3.0"
 
 // Float Slice should be sorted for best results.
 // Replaces points within delta of eachother by it's average.
@@ -38,6 +38,44 @@ func Cluster(scatter []float64, delta float64) []float64 {
   }
 
   return cluster
+}
+
+func Agglomerate(scatter []float64, delta float64) []float64 {
+  size := len(scatter)
+  cluster := make([]float64, size)
+  var index, i, j int
+  var previous, avg, n, a, b float64
+  for {
+    index, previous = 0, 0.0
+    for i=0; i<size; i++ {
+      a = scatter[i]
+      avg, n = 0.0, 0.0
+      for j=0; j<size; j++ {
+        if i==j {continue}
+        b = scatter[j]
+        if math.Abs(math.Log(a/b)) < delta {
+          avg += math.Log(b)
+          n += 1.0
+        }
+      }
+      if n == 0.0 {
+        avg = a
+      } else {
+        avg += math.Log(a)
+        n += 1.0
+        avg = math.Exp(avg/n)
+      }
+      if avg != previous {
+        cluster[index] = avg
+        index += 1
+      }
+      previous = avg
+    }
+    if index == size { break }
+    for i=0; i<index; i++ { scatter[i] = cluster[i] }
+    size = index
+  }
+  return scatter[0:size]
 }
 
 type by_near struct {
